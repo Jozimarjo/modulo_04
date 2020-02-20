c1172be898f8e8374dacbeb2546c1226305a5e26
Jozimar <jozimarjo@gmail.com>
2020-02-20 00:17:18 -0400

    iniciando app react

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports ={
+    presets:[
+        "@babel/preset-env",/** responsavel por alterar as functions do js que o navegador n entende, arrow function, import e exports */
+        "@babel/preset-react"/** transformar as coisas q o navegador n entende do react como o jsx */
+    ]
+}
